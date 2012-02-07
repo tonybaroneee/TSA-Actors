ba6f83b441dbc55ed3cf6518063803d1574db9f9
tonybaroneee <tonybaroneee@gmail.com>
2012-02-07 12:56:08 -0500

    Setup Java Project

---
+++ src/Jail.java
@@ -0,0 +1,16 @@
+/*
+ * File: Jail.java
+ * Date: 2/7/2012
+ */
+
+/**
+ * All passengers that fail the security check are sent here.
+ * 
+ * @author Anthony Barone
+ * @author Ryan Clough
+ * @author Rebecca Dudley
+ * @author Ryan Mentley
+ */
+public class Jail {
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * File: Main.java
+ * Date: 2/7/2012
+ */
+
+/**
+ * The main execution point for our TSA Airport Security simulation.
+ * 
+ * @author Anthony Barone
+ * @author Ryan Clough
+ * @author Rebecca Dudley
+ * @author Ryan Mentley
+ */
+public class Main {
+	public static void main( String[] args ) {
+		
+	}
+}
