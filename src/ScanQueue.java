4bed82bf1e23ddf303cc4850dd1259d8d1530941
Ryan Mentley <ryan@ryanmentley.com>
2012-02-14 11:36:52 -0500

    Fixed instanceof testing in ScanQueue

--- src/ScanQueue.java
+++ src/ScanQueue.java
@@ -42,52 +42,49 @@ public class ScanQueue extends UntypedActor {
 		if ( msg instanceof Passenger ) {
 			System.out.println(INDENT + "Queue " + position + ": Passenger " + 
 					((Passenger) msg).getName() + " arrives in line");
-
-			if ( msg instanceof Passenger && !closeMsgReceived ) {
-				// If msg is a Passenger, immediately send their luggage off to BaggageScan 
-				// and send them to the BodyScan if it's in a 'ready' state. Otherwise, add 
-				// them to the FIFO wait queue to be notified when the BodyScan requests the 
-				// next passenger to be scanned.
+			// If msg is a Passenger, immediately send their luggage off to BaggageScan 
+			// and send them to the BodyScan if it's in a 'ready' state. Otherwise, add 
+			// them to the FIFO wait queue to be notified when the BodyScan requests the 
+			// next passenger to be scanned.
+			System.out.println(INDENT + "Queue " + position + ": Passenger " + 
+					((Passenger) msg).getName() + " baggage placed on scanner");
+			bagScanner.tell( msg );
+			if ( bodyScannerReady ) {
 				System.out.println(INDENT + "Queue " + position + ": Passenger " + 
-						((Passenger) msg).getName() + " baggage placed on scanner");
-				bagScanner.tell( msg );
-				if ( bodyScannerReady ) {
-					System.out.println(INDENT + "Queue " + position + ": Passenger " + 
-							((Passenger) msg).getName() + " enters the body scanner");
-					bodyScanner.tell( msg, getContext() );
-					bodyScannerReady = false;
-				} else {
-					passengersWaiting.add( 0, (Passenger)msg );
-				}
-			} else if ( msg instanceof NextMsg ) {
-				// If msg is a NextMsg, the body scanner is marked ready if no passengers 
-				// are waiting. Otherwise, the first passenger is sent into the scanner.
-				if ( passengersWaiting.isEmpty() ) {
-					bodyScannerReady = true;
-					// Check if we are trying to shutdown, this would be the right time
-					if ( closeMsgReceived ) {
-						bodyScanner.tell( new CloseMsg() );
-						this.getContext().stop();
-					}
-				} else {
-					Passenger p = passengersWaiting.remove( 0 );
-					bodyScanner.tell( p, getContext() );
-				}
-			} else if ( msg instanceof CloseMsg ) {
-				// If msg is a CloseMsg, message is relayed to the baggage scanner 
-				// immediately, and to the body scanner if it is in the ready state. 
-				// If the body scanner is still processing a passenger, and/or if 
-				// passengers are waiting in this queue, the CloseMsg must be deferred 
-				// until all passengers have been through the body scanner.
-				closeMsgReceived = true;
-				bagScanner.tell( msg );
-				if ( bodyScannerReady && passengersWaiting.isEmpty() ) {
-					System.out.println(INDENT + "Queue " + position + 
-							" Close sent to body scanner");
-					bodyScanner.tell( msg );
+						((Passenger) msg).getName() + " enters the body scanner");
+				bodyScanner.tell( msg, getContext() );
+				bodyScannerReady = false;
+			} else {
+				passengersWaiting.add( 0, (Passenger)msg );
+			}
+		} else if ( msg instanceof NextMsg ) {
+			// If msg is a NextMsg, the body scanner is marked ready if no passengers 
+			// are waiting. Otherwise, the first passenger is sent into the scanner.
+			if ( passengersWaiting.isEmpty() ) {
+				bodyScannerReady = true;
+				// Check if we are trying to shutdown, this would be the right time
+				if ( closeMsgReceived ) {
+					bodyScanner.tell( new CloseMsg() );
 					this.getContext().stop();
-					System.out.println(INDENT + "Queue " + position + " Closed");
 				}
+			} else {
+				Passenger p = passengersWaiting.remove( 0 );
+				bodyScanner.tell( p, getContext() );
+			}
+		} else if ( msg instanceof CloseMsg ) {
+			// If msg is a CloseMsg, message is relayed to the baggage scanner 
+			// immediately, and to the body scanner if it is in the ready state. 
+			// If the body scanner is still processing a passenger, and/or if 
+			// passengers are waiting in this queue, the CloseMsg must be deferred 
+			// until all passengers have been through the body scanner.
+			closeMsgReceived = true;
+			bagScanner.tell( msg );
+			if ( bodyScannerReady && passengersWaiting.isEmpty() ) {
+				System.out.println(INDENT + "Queue " + position + 
+						" Close sent to body scanner");
+				bodyScanner.tell( msg );
+				this.getContext().stop();
+				System.out.println(INDENT + "Queue " + position + " Closed");
 			}
 		}
 	}
