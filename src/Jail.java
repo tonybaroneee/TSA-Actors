2ee312f4ce4fcfb77883e0a3ed5b50892a167966
Ryan Mentley <ryan@ryanmentley.com>
2012-02-14 15:53:14 -0500

    Fixed dumb bug + refactored Jail

--- src/Jail.java
+++ src/Jail.java
@@ -23,8 +23,7 @@ public class Jail extends UntypedActor {
     public void onReceive( final Object msg ) throws Exception {
         // If msg is a Passenger (someone being sent to Jail), add to prisoner list.
         if ( msg instanceof Passenger ) {
-        	System.out.println(INDENT + "Jail: Passenger " + ((Passenger) msg).getName() +
-        			" jailed" );
+            printMsg("Passenger " + ((Passenger) msg).getName() + " jailed" );
             prisoners.add( (Passenger)msg );
         }
 
@@ -32,18 +31,26 @@ public class Jail extends UntypedActor {
         // count of close messages received and check to see if all have closed.
         if ( msg instanceof CloseMsg ) {
             numCloseMsgsReceived++;
-            System.out.println(INDENT + "Jail: Close received (" + numCloseMsgsReceived +
-            		" of " + TestBedConstants.NUM_LINES + " lines)" );
+            printMsg("Close received (" + numCloseMsgsReceived +
+                    " of " + TestBedConstants.NUM_LINES + " lines)" );
             if ( numCloseMsgsReceived == TestBedConstants.NUM_LINES ) {
                 // All security stations have shut down, terminate Jail.
-            	System.out.println(INDENT + "Jail: Incarcerated Passengers");
-            	for (Passenger p : prisoners){
-            		System.out.println(INDENT + "      Passenger " + p.getName());
-            	}
+                printMsg("Incarcerated Passengers");
+                for (Passenger p : prisoners) {
+                    System.out.println(INDENT + "      Passenger " + p.getName());
+                }
                 getContext().stop();
-                System.out.println("INDENT + Jail: Closed");
+                printMsg("Closed");
             }
         }
     }
-
+    
+    /**
+     * Prints a properly indented and labeled message
+     * 
+     * @param msg the message to print
+     */
+    private void printMsg(String msg) {
+        System.out.println(INDENT + "Jail: " + msg);
+    }
 }
